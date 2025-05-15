b3c2268cbb454ccf38610496b7a0380506b56ce6
Giovanni <dibellogio@gmail.com>
2025-05-15 15:07:02 +0200

    add page controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        // $movies = Movie::all();
-
-        return view('home');
+        $trains = Train::query()->orderBy("departure_time")->get();
+        // dd($trains);
+        return view('home', compact("trains"));
     }
 
     public function ex()
